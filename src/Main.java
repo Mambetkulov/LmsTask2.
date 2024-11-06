c34329a212612b0702f641a19fee3e35b03663cb
Mambetkulov <muganm320@gmail.com>
2024-11-07 01:00:04 +0600

    Lms

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        MyClass деген класс тузунуз
+//        Ал класста озунуз жонундо маалымат сактаган полелер болсун(атыныз, фамилияныз, жашыныз, Peaksoft то катышкан
+//        сабактарыныз(массив), жакшы коргон тамагыныз)
+//        Параметри эки башка болгон эки конструктор тузунуз
+//        MyClassтын 2 объектисин тузуп, конструктор аркылуу маани бериниз
+//        Эки объектке эки башка конструктор иштесин.
+//        Объекттердин маанилерин консольго чыгарыныз.
+
+        MyClass first = new MyClass();
+        first.name = "Aibek";
+        first.Surname = "Duiso uulu";
+        first.age = 19;
+        first.courses = new String[]{"variables","loop","if conditions ","methods"};
+        first.meals = "manty";
+
+        System.out.println(first.name + "\n" + first.Surname + "\n" +
+                         first.age + "\n" + Arrays.toString(first.courses) + "\n" + first.meals);
+
+        String[] array = new String[]{"Arrays","String methods","if else"};
+        MyClass second = new MyClass("Mike","Tyson",30,array,"hot dog");
+
+        System.out.println("-------------------------------------------------------------------");
+
+        System.out.println(second.name + "\n" + second.Surname + "\n" +
+                second.age + "\n" + Arrays.toString(second.courses) + "\n" + second.meals);
+        System.out.println("");
+
+        Random r = new Random();
+          int aibek = r.nextInt(1,100);
+          int tyson = r.nextInt(1,100);
+        System.out.println("fight 😼😼😼");
+
+          if(aibek < tyson){
+              System.out.println( "tyson win 😎" +"\n" + "aibek loser 😒");
+          }
+          else{
+              System.out.println("aibek win 😎" +"\n" + "tyson loser 😒");
+          }
+
+
+
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class MyClass {
+    String name;
+    String Surname ;
+    int age ;
+    String[] courses;
+    String meals;
+
+    public MyClass(){
+
+    }
+
+    public MyClass(String name , String Surname , int age , String[] courses , String meals){
+        this.name = name;
+        this.Surname = Surname;
+        this.age = age;
+        this.courses = courses;
+        this.meals = meals;
+
+    }
+
+
+
+
+
+}
